ccd926bce4e4156ce23b4fd641b2c1ed6cda786b
farmerzhang1 <farmerzhang1@mail.ustc.edu.cn>
2021-09-13 21:32:33 +0800

    很懒惰地用了InMemoryUploadedFile

--- fm/views.py
+++ fm/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from .forms import *
 # Create your views here.
 def test1Page(request):
     return render(request, 'fm/test1.html')
@@ -15,3 +16,23 @@ def sub2(request):
 
 def sub3(request):
     return render(request, 'fm/3.html')
+
+def upload_file(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            # handle_uploaded_file(request.FILES['file'])
+            print(request.FILES['file'])
+            print (form.cleaned_data)
+            request.session['temp_data'] = { 'number' : 1234 }
+            return redirect('result')
+        else:
+            print('not valid!!!!')
+            print(form.errors)
+    else:
+        form = UploadFileForm()
+    return render(request, 'fm/upload.html', {'form': form})
+
+def result(request):
+    print(request.session['temp_data'])
+    return render(request, 'fm/result.html', {'number': 1})
