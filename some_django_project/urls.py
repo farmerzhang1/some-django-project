cc8abd1d9b9223be97cb8fc1eb8daa26a0bcef72
farmerzhang1 <farmerzhang1@mail.ustc.edu.cn>
2021-09-26 17:19:44 +0800

    change urls

--- some_django_project/urls.py
+++ some_django_project/urls.py
@@ -20,8 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static #don't use this when real development
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', include('accounts.urls')),
-    path('fm/', include('fm.urls'))
+    path('', include('fm.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
