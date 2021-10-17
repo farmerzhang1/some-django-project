b65f6e2781e082cd5bb93e73349e3b6661ee0cfe
farmerzhang1 <farmerzhang1@mail.ustc.edu.cn>
2021-10-17 11:30:21 +0800

    textarea

--- fm/forms.py
+++ fm/forms.py
@@ -2,4 +2,5 @@ from django import forms
 
 class UploadFileForm(forms.Form):
     # title = forms.CharField(max_length=50)
-    file = forms.FileField()
+    file = forms.FileField(widget=forms.FileInput(attrs={'class': 'file-upload-field'}))
+    text = forms.CharField(widget=forms.Textarea(attrs={'id': 'js-textarea'}))
