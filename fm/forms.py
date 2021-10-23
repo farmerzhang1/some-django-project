00bfdb5afbc2931054f680f3498b11eb8361780b
farmerzhang1 <zhang2000dc@gmail.com>
2021-10-23 21:50:35 +0800

    hide codemirror when no input

--- fm/forms.py
+++ fm/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 class UploadFileForm(forms.Form):
     # title = forms.CharField(max_length=50)
-    file = forms.FileField(widget=forms.FileInput(attrs={'class': 'file-upload-field'}))
+    file = forms.FileField(widget=forms.FileInput(attrs={'class': 'file-upload-field', 'id': 'file1'}))
     text = forms.CharField(widget=forms.Textarea(attrs={'id': 'js-textarea'}))
