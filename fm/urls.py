6d5942a1bbe4b04296a6740218873893efc72c33
farmerzhang1 <farmerzhang1@mail.ustc.edu.cn>
2021-12-19 20:52:26 +0800

    fix url

--- fm/urls.py
+++ fm/urls.py
@@ -5,10 +5,10 @@ urlpatterns = [
     path('tamarin/', run_tamarin, name='tamarin'),
     path('load_file/', load_file, name='load_file'),
     path('test2', test2Page, name = 'test2'),
-    path('fv', sub1, name = 'formal verification'),
+    path('fv_old', sub1, name = 'formal verification old'),
     path('dp', sub2, name = 'defect prediction'),
     path('dd', sub3, name = 'defect detection'),
     path('upload', upload_file, name = 'upload'),
-    path('upload_new', upload_new, name = 'upload_new'),
+    path('fv', upload_new, name = 'formal verification'),
     path('result', result, name = 'result'),
 ]
